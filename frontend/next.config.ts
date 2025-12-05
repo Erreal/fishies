6d0eaeecc9f42214fd8cfbabf620fa37db2782f7
Mikhail Zakharov <erreal@yandex.ru>
2025-12-05 16:32:30 +0300

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  env: {
+    PORT: process.env.PORT,
+    HOSTNAME: process.env.HOSTNAME,
+  },
 };
 
 export default nextConfig;
